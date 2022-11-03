f61570d6f91b814374ccfebc3fada76812a8c07f
Ant2211 <turpak85@gmail.com>
2022-11-03 18:49:22 +0300

    added phonetics

--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,19 @@ export default function Results(props) {
     if(props.results){
 return(
     <div className="results"> 
-<h2> {props.results.word} </h2>
+<h1> {props.results.word} </h1>
+
+<div> {props.results.phonetics.map(function (phonetics, index) {
+    
+    return (
+        <div className="my-3" key={index}>
+           <a href={phonetics.audio} target="_blank" rel="noreferrer" className="me-3">🔈</a>
+           {phonetics.text}
+            </div>
+    );
+    
+})} 
+</div>
 {props.results.meanings.map(function (meaning, index) {
     return (
         <div key={index}>
